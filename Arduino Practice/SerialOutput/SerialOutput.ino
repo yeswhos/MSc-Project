2120bd7d3e3fa05178117b7fc0e5d0ecfdf496a4
yeswhos <yeswhos@outlook.com>
2020-06-13 16:26:33 +0100

    serial output

---
+++ Arduino Practice/SerialOutput/SerialOutput.ino
@@ -0,0 +1,24 @@
+
+int pushButton = 14;
+int buttonState = 0;
+int beforeState = 0;
+// the setup routine runs once when you press reset:
+void setup() {
+  // initialize serial communication at 9600 bits per second:
+  Serial.begin(9600);
+  // make the pushbutton's pin an input:
+  pinMode(pushButton, INPUT);
+}
+
+// the loop routine runs over and over again forever:
+void loop() {
+  // read the input pin:
+  buttonState = digitalRead(pushButton);
+  // print out the state of the button:
+  if(buttonState == 1 && beforeState == 0){
+    Serial.print("Serial Output: ");
+    Serial.println(buttonState);
+    delay(1); 
+  }
+  beforeState = buttonState;
+}
