2c2965c93f1a84c20b9f5c4cc52ecf594f20b984
yeswhos <yeswhos@outlook.com>
2020-06-13 15:49:22 +0100

    blink basic

--- Arduino Practice/blink/blink.ino
+++ Arduino Practice/blink/blink.ino
@@ -1,12 +1,12 @@
-//最先执行
+//最先执行 LED_BUILTIN
 void setup() {
-  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(13, OUTPUT);
 }
 
 //会一直做下面的事情
 void loop() {
-  digitalWrite(LED_BUILTIN, HIGH);
-  delay(1000);                       
-  digitalWrite(LED_BUILTIN, LOW);    
-  delay(1000);                      
+  digitalWrite(13, HIGH);
+  delay(100);                       
+  digitalWrite(13, LOW);    
+  delay(300);                      
 }
