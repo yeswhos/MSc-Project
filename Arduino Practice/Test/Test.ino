9108f638f810e026030434cded2025b19ceea360
yeswhos <yeswhos@outlook.com>
2020-06-14 20:45:56 +0100

    update test

--- Arduino Practice/Test/Test.ino
+++ Arduino Practice/Test/Test.ino
@@ -1,21 +1,23 @@
 #include <Zumo32U4.h>
+#include<Wire.h>
 Zumo32U4LCD lcd;
 Zumo32U4ProximitySensors proxSensors;
 
 void setup() {
+  proxSensors.initFrontSensor();
   proxSensors.initThreeSensors();
 }
 
 void loop() {
   proxSensors.read();
   lcd.clear();
-  lcd.gotoXY(0,1);
+  lcd.gotoXY(0,0);
   lcd.print(proxSensors.countsLeftWithLeftLeds() );
   lcd.gotoXY(2,0);
   lcd.print(proxSensors.countsFrontWithLeftLeds() );
   lcd.gotoXY(5,0);
   lcd.print(proxSensors.countsFrontWithRightLeds() );
-  lcd.gotoXY(7,1);
+  lcd.gotoXY(7,0);
   lcd.print(proxSensors.countsRightWithRightLeds() );
   delay(100);
 }
