fee1d7e31e9e8d2b64b8e9f85510d04929fe0743
yeswhos <yeswhos@outlook.com>
2020-06-19 20:18:15 +0100

    DT 2

---
+++ Control System/Decision Tree/Demo02/Demo02/demo.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include"DecisionTree.h"
+using namespace std;
+int main() {
+    Tree* tree = new Tree;
+    cout << "Trained..........." << endl;
+
+    while (1)
+        tree->GetOutPut();
+
+    return 0;
+}
