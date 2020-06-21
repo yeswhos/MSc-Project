44eb881675d16830f7ba89a05bf2cbc26b96ed06
yeswhos <yeswhos@outlook.com>
2020-06-21 17:20:20 +0100

    map with prox sensor overlap failed

--- Arduino Practice/MapTest/MapTest.ino
+++ Arduino Practice/MapTest/MapTest.ino
@@ -7,8 +7,10 @@ Zumo32U4ProximitySensors proxSensors;
 
 //arx::map<String, int> mp {{"one", 1}, {"two", 2}};
 //arx::map<int, int> mp {{1, 10}, {2, 20}};
-arx::map<int, map<int, int>> multiMap;
+arx::map<int, int> mp;
+arx::map <int, mp > multiMap {1, {2, 20}};
 
+arx::map<int, int> mps;
 uint16_t newdata;
 
 void setup() {
@@ -17,18 +19,18 @@ void setup() {
 //  map<int, int> m;
 //  m.insert(pair<int, int>(1, 10));
 //  m.insert(pair<int, int>(2, 20));
-  
+//  mp.insert(1, 10);
+//  multiMap.insert(2, mp);
+  Serial.println(multiMap);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   proxSensors.read();
   newdata = proxSensors.countsFrontWithLeftLeds();
-//  map<int, int>::iterator pos = m.find(newdata);
-//  if(pos != m.end()){
-//    Serial.println(pos->second);    
-//  }
-  Serial.println(mp[newdata]);
+
+//  Serial.println(mp[newdata]);
+  
   Serial.println("-----------------------");
   Serial.println(newdata);
   Serial.println("-----------------------");
