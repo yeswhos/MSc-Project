96361ab1ef7ed3c82ac6c490fe0867c2e03fbd61
yeswhos <yeswhos@outlook.com>
2020-06-25 15:12:34 +0100

    update json test

--- Arduino Practice/JsonTest/JsonTest.ino
+++ Arduino Practice/JsonTest/JsonTest.ino
@@ -1,13 +1,26 @@
 #include <ArduinoJson.h>
+#include<Wire.h>
+#include<Zumo32U4.h>
+
+Zumo32U4ProximitySensors proxSensors;
+Zumo32U4LCD lcd;
+
+char json[] = "{\"sensor\": [{\"0\": \"gostright\"}, {\"1\": \"goright\"}]}";
+uint8_t value;
 void setup() {
-  Serial.begin(9600);
+  proxSensors.initFrontSensor();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+    Serial.begin(9600);
   while (!Serial) continue;
   DynamicJsonDocument doc(1024);
   // put your setup code here, to run once:
   //char json[] = "{\"sensor\":\"gps\",\"time\":1351824120,\"data\":[48.756080,2.302038]}";
 //  unsigned int json[] = "{"no surfacing": {"0": "NO", "1": {"flippers": {"0": "NO", "1": "YES"}}}}"
 //  char json[] = "{\"sensor\": {\"0\": NO, \"1\": {\"sensor2\": {\"0\": Yes, \"1\": no}}}}";
-  char json[] = "{\"sensor\": [{\"0\": \"gostright\"}, {\"1\": 2}]}";
+  char json[] = "{\"sensor\": [{\"0\": \"gostright\"}, {\"1\": \"goright\"}]}";
   //StaticJsonDocument<200> doc;
   
   //deserializeJson(doc, json);
@@ -19,15 +32,17 @@ void setup() {
     Serial.println(error.c_str());
     return;
   }
-  Serial.println("here");
-  const char* sensor = doc["sensor"][0]["0"];
+  proxSensors.read();
+  lcd.gotoXY(0, 0);
+  value = proxSensors.countsFrontWithLeftLeds();
+  lcd.print(value);
+  const char* sensor = doc["sensor"][value]["0"];
+  Serial.println(sensor);
+  delay(1000);
+  const char* sensor2 = doc["sensor"][value]["1"];
+  Serial.println(sensor2);
 //  long time          = doc["time"];
 //  double latitude    = doc["data"][0];
 //  double longitude   = doc["data"][1];
-  Serial.println(sensor);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-
+  
 }
