1923bf8bad87e3bd821e52560dbca2758301bb95
yeswhos <yeswhos@outlook.com>
2020-06-29 16:08:57 +0100

    update inside the ring, need to be tested

--- Arduino Practice/Inside_the_ring/Inside_the_ring.ino
+++ Arduino Practice/Inside_the_ring/Inside_the_ring.ino
@@ -12,6 +12,7 @@ uint16_t lineSensor2;
 uint16_t lineSensor3;
 
 const uint16_t sensorThreshold = 800;
+const uint16_t turnSpeed = 200;
 
 boolean warnRight = false;
 boolean warnLeft = false;
@@ -27,6 +28,31 @@ void setup() {
   
 }
 
+void turnRight()
+{
+  motors.setSpeeds(turnSpeed, -turnSpeed);
+  delay(5000);
+}
+
+void turnLeft()
+{
+  motors.setSpeeds(-turnSpeed, turnSpeed);
+  delay(5000);
+}
+
+void turnAround()
+{
+  motors.setSpeeds(turnSpeed, -turnSpeed);
+  delay(10000);
+}
+
+void goStraight()
+{
+  motors.setSpeeds(200, 200);
+  //delay(1000);
+}
+
+
 void loop() {
   Serial.begin(9600);
   lineSensors.read(lineSensorValues);
@@ -37,24 +63,18 @@ void loop() {
   warnRight = (lineSensor1 <= sensorThreshold) && (lineSensor3 >= sensorThreshold);
   warnLeft = (lineSensor1 >= sensorThreshold) && (lineSensor3 <= sensorThreshold);
   warnCenter = (lineSensor1 <= sensorThreshold) && (lineSensor3 <= sensorThreshold);
-  do{
-    lineSensors.read(lineSensorValues);
-    lineSensor1 = lineSensorValues[0];
-    lineSensor2 = lineSensorValues[2];
-    lineSensor3 = lineSensorValues[4];
-    Serial.println(warnRight);
-    Serial.println(lineSensor1);
-  }while(warnRight);
-//  while(warnRight){
-//    lineSensors.read(lineSensorValues);
-//    Serial.println(warnRight);
-//    Serial.println(lineSensor1);
-//  }
-  if(warnLeft){
+  if(warnRight){
+    Serial.println("右侧");
+    turnRight();
+  }
+  else if(warnLeft){
     Serial.println("左侧");
+    turnLeft();
   }
   else if(warnCenter){
     Serial.println("中间");
+    turnAround();
   }
+  goStraight();
   Serial.println("直走");
 }
