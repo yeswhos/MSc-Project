7d84c2ad05c4de642db6394a091c977bf08c1943
yeswhos <yeswhos@outlook.com>
2020-07-01 16:48:37 +0100

    update test prox sensor

--- Arduino Practice/Test/Test.ino
+++ Arduino Practice/Test/Test.ino
@@ -16,12 +16,15 @@ void loop() {
   /*  frontleft = map(proxSensors.countsLeftWithLeftLeds(), 0, 6, 0, 3);
   lcd.print(frontleft);*/
   lcd.print(proxSensors.countsLeftWithLeftLeds());
+//  lcd.print(map(proxSensors.countsLeftWithLeftLeds(), 0, 6, 0, 3) );
   lcd.gotoXY(2,0);
   frontleft = map(proxSensors.countsFrontWithLeftLeds(), 0, 6, 0, 3);
   lcd.print(frontleft);
   lcd.gotoXY(5,0);
-  lcd.print(proxSensors.countsFrontWithRightLeds() );
+  //lcd.print(proxSensors.countsFrontWithRightLeds() );
+  lcd.print(map(proxSensors.countsFrontWithRightLeds(), 0, 6, 0, 3) );
   lcd.gotoXY(7,0);
   lcd.print(proxSensors.countsRightWithRightLeds() );
+  //lcd.print(map(proxSensors.countsRightWithRightLeds(), 0, 6, 0, 3) );
   delay(100);
 }
